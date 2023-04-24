060d2d88d7dc8d53176a1980940f4b10f1057936
Myridwan <92353970+MyRidwan@users.noreply.github.com>
2023-04-24 10:01:06 +0700

    Add files via upload

---
+++ websocket/insshws.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Proxy For WS
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+echo "Installing SSH Websocket by RstoreVPN" | lolcat
+echo "Progress..." | lolcat
+sleep 3
+cd
+
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/dropbear-ws.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-stunnel
+
+chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-stunnel
+
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+
+systemctl daemon-reload
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
+systemctl enable ws-stunnel.service
+systemctl start ws-stunnel.service
+systemctl restart ws-stunnel.service
+
---
+++ websocket/nontls.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Proxy For NO TLS
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+echo "Installing SSH Websocket None TLS by RstoreVPN " | lolcat
+echo "Progress..." | lolcat
+sleep 3
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-nontls.py
+chmod +x /usr/local/bin/ws-nontls
+
+# Installing Service
+cat > /etc/systemd/system/ws-nontls.service << END
+[Unit]
+Description=Python Proxy Mod By Arh-Project
+Documentation=https://t.me/r1f4n_1122
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-nontls
+systemctl restart ws-nontls
+
